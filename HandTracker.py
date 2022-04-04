726546c12e9e765a0e7b7791202bb885e8ff9d73
Vibhu Gupta <vibhu.gupta2@student.amity.edu>
2022-04-04 22:31:21 +0530

    adding helper function

--- HandTracker.py
+++ HandTracker.py
@@ -154,6 +154,35 @@ class HandDetector:
         else:
             return length, info
 
+def findPosition(self, img, handNo=0, draw=True):
+    xList = []
+    yList = []
+    bbox = []
+    self.lmList = []
+    if self.results.multi_hand_landmarks:
+        myHand = self.results.multi_hand_landmarks[handNo]
+        for id, lm in enumerate(myHand.landmark):
+            # print(id, lm)
+            h, w, c = img.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            xList.append(cx)
+            yList.append(cy)
+            # print(id, cx, cy)
+            self.lmList.append([id, cx, cy])
+            if draw:
+                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+ 
+    xmin, xmax = min(xList), max(xList)
+    ymin, ymax = min(yList), max(yList)
+    bbox = xmin, ymin, xmax, ymax
+ 
+    if draw:
+        cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+        (0, 255, 0), 2)
+ 
+    return self.lmList, bbox
+
+
 def main():
     cap = cv2.VideoCapture(0)
     detector = HandDetector(detectionCon=0.8, maxHands=2)
